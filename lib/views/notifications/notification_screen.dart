fc20319193e10b272281d536f601564aa30a985e
jassimpv <pv.jassim@gmail.com>
2025-06-24 11:40:18 +0400

    Refactor: Simplify layout structure in DashboardScreen and enhance notification item with zoom animation

--- lib/views/notifications/notification_screen.dart
+++ lib/views/notifications/notification_screen.dart
@@ -4,6 +4,7 @@ import 'package:collect/utils/colors_utils.dart';
 import 'package:collect/utils/sized_box_extension.dart';
 import 'package:collect/utils/textstyle_input.dart';
 import 'package:collect/views/widget/custom_app_bar.dart';
+import 'package:collect/views/widget/zoom_tap.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -68,59 +69,61 @@ class _NotificationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: StyleUtils.cardView(),
-      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-      child: Row(
-        children: [
-          Container(
-            width: 40,
-            height: 40,
-            decoration: BoxDecoration(shape: BoxShape.circle),
-            child: Center(
-              child: Container(
-                width: 50,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: ColorUtils.themeColor.withValues(alpha: 0.1),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Center(
-                  child: Icon(
-                    notificationTypes == NotificationTypes.rejected
-                        ? Icons.close
-                        : Icons.done,
-                    size: 20,
-                    color: ColorUtils.linkColor,
+    return ZoomTapAnimation(
+      child: Container(
+        decoration: StyleUtils.cardView(),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Row(
+          children: [
+            Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(shape: BoxShape.circle),
+              child: Center(
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: ColorUtils.themeColor.withValues(alpha: 0.1),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Icon(
+                      notificationTypes == NotificationTypes.rejected
+                          ? Icons.close
+                          : Icons.done,
+                      size: 20,
+                      color: ColorUtils.linkColor,
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          10.widthBox,
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  notificationTypes == NotificationTypes.rejected
-                      ? "Rejected"
-                      : "Approved",
-                  style: StyleUtils.kTextStyleSize14Weight500(
-                    color: Colors.black,
+            10.widthBox,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    notificationTypes == NotificationTypes.rejected
+                        ? "Rejected"
+                        : "Approved",
+                    style: StyleUtils.kTextStyleSize14Weight500(
+                      color: Colors.black,
+                    ),
                   ),
-                ),
-                Text(
-                  notifications?.message ?? "",
-                  style: StyleUtils.kTextStyleSize14Weight400(
-                    color: Colors.black.withValues(alpha: 0.5),
+                  Text(
+                    notifications?.message ?? "",
+                    style: StyleUtils.kTextStyleSize14Weight400(
+                      color: Colors.black.withValues(alpha: 0.5),
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
